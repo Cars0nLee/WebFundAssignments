f1a766636202952b4017c7d384ccb0ee49e0fdc8
Carson Lee <heesung199530@gmail.com>
2021-09-17 15:51:13 -0400

    Button

---
+++ ButtonClicker/btn.js
@@ -0,0 +1,11 @@
+function logout(element) {
+    element.innerText = "Logout"
+}
+
+function remove(element) {
+    element.remove();
+}
+
+function alertMethod(element) {
+    alert("Ninja was liked!");
+}
