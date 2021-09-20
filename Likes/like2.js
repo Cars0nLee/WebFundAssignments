c38ab48cbea1147a5bec7fa80a392177d8c5a6c9
Carson Lee <heesung199530@gmail.com>
2021-09-20 18:02:03 -0400

    Likes

---
+++ Likes/like2.js
@@ -0,0 +1,21 @@
+var likes = 0;
+
+function count() {
+    likes++;
+    document.querySelector("#likes").innerText = likes + " like(s)";
+}
+
+
+var likes1 = 0;
+
+function count1() {
+    likes1++;
+    document.querySelector("#likes1").innerText = likes1 + " like(s)";
+}
+
+var likes2 = 0;
+
+function count2() {
+    likes2++;
+    document.querySelector("#likes2").innerText = likes2 + " like(s)";
+}
